63c6901954c8d08f527f3447680a9c8e25bf78d7
elevenqtwo <elevenqtwo@gmail.com>
2023-11-22 18:14:10 +0300

    page layout on spring, minor fixes

--- src/main/java/com/example/Archive/demo/controller/UsersController.java
+++ src/main/java/com/example/Archive/demo/controller/UsersController.java
@@ -5,9 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UsersController {
-
     @GetMapping("/register")
     public String getRegisterPage() {
-        return "registration";
+        return "registration_page";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login_page";
+    }
+
+    @GetMapping("/docks")
+    public String getDocksPage() {
+        return "docks_page";
+    }
+
+    @GetMapping("/starting_page")
+    public String getStartingPage() {
+        return "index";
     }
 }
